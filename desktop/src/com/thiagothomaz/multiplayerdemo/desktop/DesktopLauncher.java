db710d8608dfd3c109b5c956a5d7f935996b3c66
= <thiagorthomaz@gmail.com>
2016-07-22 21:05:04 -0300

    Hello World using libgdx

---
+++ desktop/src/com/thiagothomaz/multiplayerdemo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.thiagothomaz.multiplayerdemo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.thiagothomaz.multiplayerdemo.MultiplayerDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MultiplayerDemo(), config);
+	}
+}
