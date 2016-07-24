358a416fe453b5790967ef13e17caf2a2fc7769f
= <thiagorthomaz@gmail.com>
2016-07-24 18:10:45 -0300

    Client now caan communicate directly to the server; Send messages from one socket to other sockets.

--- core/src/com/thiagothomaz/multiplayerdemo/MultiplayerDemo.java
+++ core/src/com/thiagothomaz/multiplayerdemo/MultiplayerDemo.java
@@ -6,8 +6,12 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import io.socket.client.IO;
 import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
 
 public class MultiplayerDemo extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -19,13 +23,61 @@ public class MultiplayerDemo extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		connectSocket();
+        configSocketEvents();
+
 	}
 
-	public void connectSocket() {
+    public void configSocketEvents() {
+        this.socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                Gdx.app.log("SocketIO", "Connected");
+            }
+        }).on("socketID", new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+
+                try {
+                    JSONObject data = (JSONObject) args[0];
+                    String id = data.getString("id");
+                    Gdx.app.log("SocketIO", "My id is: " + id);
+                } catch (JSONException e) {
+                    Gdx.app.log("SocketIO", "Error getting ID");
+                }
+
+            }
+        }).on("newPlayer", new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                try {
+                    JSONObject data = (JSONObject) args[0];
+                    String id = data.getString("id");
+                    Gdx.app.log("SocketIO", "New player connected: " + id);
+                } catch (JSONException e) {
+                    Gdx.app.log("SocketIO", "Error getting new player ID");
+                }
+            }
+        }).on("disconnectedPlayer", new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                try {
+                    JSONObject data = (JSONObject) args[0];
+                    String id = data.getString("id");
+                    Gdx.app.log("SocketIO", "Player disconnected: " + id);
+                } catch (JSONException e) {
+                    Gdx.app.log("SocketIO", "Error getting player disconnected ID");
+                }
+            }
+        });
+
+    }
+
+    public void connectSocket() {
 		try{
 
 			this.socket = IO.socket("http://localhost:8080");
 			this.socket.connect();
+
 
 		}catch (Exception e) {
 			System.out.println(e);
--- server/index.js
+++ server/index.js
@@ -8,9 +8,22 @@ server.listen(8080, function(){
 });
 
 
+//Accept new client's connections
 io.on('connection', function(socket){
-	console.log('Player Connected');
+	console.log('Player Connected. ID: ' + socket.id);
+
+
+    //Send a ID for the client connected
+    socket.emit('socketID', {id:socket.id});
+
+    //Tell to other sockets that this one is now connected
+    socket.broadcast.emit("newPlayer", {id:socket.id});
+
+    //Disconnect client
 	socket.on('disconnect', function(){
-		console.log('Player Disconnected');
+		console.log('Player Disconnected. ID: ' + socket.id);
+
+		//Tell to other sockets that this one is now disconnected
+		socket.broadcast.emit("disconnectedPlayer", {id:socket.id});
 	});
 });
