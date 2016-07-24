0c3863304a1740f1cb304ece77c976717443a7d6
= <thiagorthomaz@gmail.com>
2016-07-24 17:42:14 -0300

    Created a very simple server that clients can connect and disconect

--- core/src/com/thiagothomaz/multiplayerdemo/MultiplayerDemo.java
+++ core/src/com/thiagothomaz/multiplayerdemo/MultiplayerDemo.java
@@ -6,14 +6,31 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import io.socket.client.IO;
+import io.socket.client.Socket;
+
 public class MultiplayerDemo extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	private Socket socket;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		connectSocket();
+	}
+
+	public void connectSocket() {
+		try{
+
+			this.socket = IO.socket("http://localhost:8080");
+			this.socket.connect();
+
+		}catch (Exception e) {
+			System.out.println(e);
+
+		}
 	}
 
 	@Override
